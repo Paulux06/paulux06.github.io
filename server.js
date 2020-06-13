fbb5c50be59e3bb5497ce6eebad40f8554c357ff
Paulux <paulux.polux@gmail.com>
2020-06-13 17:36:07 +0200

    yes

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var url = require("url");
+var fs   = require('fs');
+
+http.createServer(function(req, res) {
+    req.url = '.'+req.url;
+    console.log(req.url);
+    if (req.url == './')
+    {
+        res.end(fs.readFileSync('./index.html'));
+        return;
+    }
+    res.end(fs.readFileSync(req.url));
+    
+}).listen(80);
