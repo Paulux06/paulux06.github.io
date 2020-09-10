6feb2c8ca00373e6d68b0d9e8a9bccff688a8da8
Paulux <45801364+Paulux06@users.noreply.github.com>
2020-09-10 22:34:18 +0200

    Trying new design

---
+++ header.js
@@ -0,0 +1,21 @@
+function setupHeader() {
+    var title = document.getElementById("header-promo-pseudo");
+    setTimeout(()=>{
+        title.style.transform = "translate(0px, 0vh) rotate(0deg) scale(0.8)";
+    }, 100);
+    setTimeout(()=>{
+        title.style.transform = "translate(0px, 0vh) rotate(0deg) scale(1)";
+    }, 1000);
+    document.getElementById("header-promo-color1").style.width = "20vw";
+    document.getElementById("header-promo-color2").style.width = "20vw";
+    document.getElementById("header-promo-color3").style.width = "20vw";
+    document.getElementById("header-promo-color4").style.width = "20vw";
+    document.getElementById("header-promo-color5").style.width = "20vw";
+    document.getElementById("header-promo-welcome").style.transform = "translate(0px, 0vh) rotate(0deg) scale(1)";
+
+    document.documentElement.style.setProperty("--header-option-scale", "1");
+}
+
+window.addEventListener("scroll", (event)=> {
+    document.documentElement.style.setProperty("--header-promo-height", Math.max(50 - pageYOffset / 10, 0).toString() + "vh");
+})
--- pages.js
+++
@@ -1,6 +0,0 @@
-function clearPage()
-{
-    var page = document.getElementById("content-container");
-    while (page.firstChild)
-        page.removeChild(page.firstChild);
-}
